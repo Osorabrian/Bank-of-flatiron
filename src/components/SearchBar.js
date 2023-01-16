b403ca3c321b16be2ab4e166206556a80a0c7eee
Osorabrian <osorabrian@gmail.com>
2023-01-16 20:20:31 +0300

    add comments to search form component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react"
 import { Button,Input,Form } from "reactstrap";
 
+// create search form component
 function SearchBar({filterTransaction}){
 
+    // search variable and setter function 
     const [search, setSearch] = useState("")
 
+    // function that calls a callback function to filter data
     function handleSubmit(e){
         e.preventDefault()
         filterTransaction(search)
